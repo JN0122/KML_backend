c2dbcf3ad1ebfe938dbd78940d9bae7e835e311f
Mateusz Grochowski <groszek20027@gmail.com>
2024-05-08 20:12:23 +0200

    Moved database stuff

--- main.py
+++ main.py
@@ -1,27 +1,9 @@
 from fastapi import FastAPI
 from routers import time_frame_router
 
-# Import necessary modules and classes
 from fastapi import FastAPI, Depends, HTTPException
-from sqlalchemy import create_engine, Column, Integer, String
-import sqlalchemy
-from sqlalchemy.orm import sessionmaker, Session
-from pydantic import BaseModel
 
-
-app = FastAPI(
-    swagger_ui_parameters={
-        "syntaxHighlight": True
-    }
-)
-
-
-# Database setup
-DATABASE_URL = "sqlite:///./test.db"
-engine = create_engine(DATABASE_URL)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-Base = sqlalchemy.orm.declarative_base()
-
+app = FastAPI(swagger_ui_parameters={"syntaxHighlight": True})
 
 app.include_router(time_frame_router.router)
 
