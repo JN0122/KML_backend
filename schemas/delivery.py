3eaddafa367368ff9ae3b7634b6be71b1deb5bef
Mateusz Grochowski <groszek20027@gmail.com>
2024-05-13 16:49:37 +0200

    Created delivery DTO

---
+++ schemas/delivery.py
@@ -0,0 +1,21 @@
+from pydantic import BaseModel
+from datetime import date, time
+
+
+class DeliveryBase(BaseModel):
+    date: date
+    time: time
+    ulg95: int 
+    dk: int 
+    ultsu: int 
+    ultdk: int
+    total: int
+
+class DeliveryCreate(DeliveryBase):
+    pass
+
+class Delivery(DelivereBase):
+    id: int
+
+    class Config:
+        orm_mode = True
