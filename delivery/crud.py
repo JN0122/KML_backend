1df1588105531c8807afda63c10e7aed5b8f8234
Mateusz Grochowski <groszek20027@gmail.com>
2024-05-15 12:23:08 +0200

    Added creation of delivery

--- delivery/crud.py
+++ delivery/crud.py
@@ -1,6 +1,6 @@
 from sqlalchemy.orm import Session
 
-from delivery import model 
+from delivery import model
 from delivery import dto
 
 
@@ -10,3 +10,19 @@ def get_delivery_by_id(db: Session, delivery_id: int):
 
 def get_deliveries(db: Session, skip: int = 0, limit: int = 100):
     return db.query(model.Delivery).offset(skip).limit(limit).all()
+
+
+def create_delivery(db: Session, delivery: dto.DeliveryCreate):
+    db_delivery = model.Delivery(
+        date=delivery.date,
+        time=delivery.time,
+        ulg95=delivery.ulg95,
+        dk=delivery.dk,
+        ultsu=delivery.ultsu,
+        ultdk=delivery.ultdk,
+        total=delivery.ulg95 + delivery.dk + delivery.ultsu + delivery.ultdk,
+    )
+    db.add(db_delivery)
+    db.commit()
+    db.refresh(db_delivery)
+    return db_delivery
