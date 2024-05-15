c9d883a8352f0a15dba60fb51778519082230870
Mateusz Grochowski <groszek20027@gmail.com>
2024-05-15 11:45:04 +0200

    Added basic get functions

---
+++ delivery/crud.py
@@ -0,0 +1,12 @@
+from sqlalchemy.orm import Session
+
+import delivery.model as model
+import delivery.dto as dto
+
+
+def get_delivery_by_id(db: Session, delivery_id: int):
+    return db.query(model.Delivery).filter(model.Delivery == delivery_id).first()
+
+
+def get_deliveries(db: Session, skip: int = 0, limit: int = 100):
+    return db.query(model.Delivery).offset(skip).limit(limit).all()
