06be9e72642ba6f1fb4d1aca5761c142e44ca866
Mateusz Grochowski <groszek20027@gmail.com>
2024-05-15 11:44:52 +0200

    Added binding for database creation

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 from fastapi import FastAPI
+import delivery.model
 from routers import time_frame_router
 
 from fastapi import FastAPI, Depends, HTTPException
+import delivery
+from database import SessionLocal, engine, Base
+
+Base.metadata.create_all(bind=engine)
 
 app = FastAPI(swagger_ui_parameters={"syntaxHighlight": True})
 
