59b4f86c39b69ca12dc3f5bd4deac1ceba58c37f
Bartosz Bizoń <bartosz.bizon17@gmail.com>
2024-05-16 16:31:18 +0200

    Tiny refactor in forecasts router

--- forecast/router.py
+++ forecast/router.py
@@ -1,16 +1,15 @@
 from fastapi import APIRouter
-from datetime import date as d
 from forecast import dto, winters_module
-from routers.delivery_router import read_all_deliveries
+from delivery.router import read_all_deliveries
 
 router = APIRouter(
-    prefix="/forecast",
-    tags=["forecast"],
+    prefix="/forecasts",
+    tags=["forecasts"],
     responses={404: {"description": "Not found"}},
 )
 
 
-@router.get("/read_all", response_model=list[dto.Forecast])
+@router.get("/", response_model=list[dto.Forecast])
 def read_all_forecasts():
     data = read_all_deliveries()
 
