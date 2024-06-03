a57585f533df4e030234c167bb23a498b8c28d91
Bartosz Bizoń <bartosz.bizon17@gmail.com>
2024-06-03 12:54:13 +0200

    Use HoltWinters instead of Winters

--- forecast/router.py
+++ forecast/router.py
@@ -4,7 +4,7 @@ from sqlalchemy.orm import Session
 
 from database.database import get_db
 from delivery import crud
-from forecast.winters.RunWinters import RunWinters
+from forecast.winters.RunModels import RunModels
 
 router = APIRouter(
     prefix="/forecasts",
@@ -13,8 +13,10 @@ router = APIRouter(
 )
 
 
-@router.get("/{station_id}")
-def calculate_forecasts_for_every_fuel(station_id: int, db: Session = Depends(get_db)):
+@router.get("/")
+def calculate_forecasts_for_every_fuel(station_id: int, forecast_len: int, db: Session = Depends(get_db)):
     data = crud.read_deliveries_for_winters(station_id=station_id, limit=350, db=db)
-    run_winters = RunWinters()
-    return run_winters.for_every_fuel(data)
+
+    run_winters = RunModels()
+    result = run_winters.holt_winters_for_every_fuel(data, forecast_len)
+    return result
--- forecastdelivery/router.py
+++ forecastdelivery/router.py
@@ -4,7 +4,7 @@ from sqlalchemy.orm import Session
 
 from database.database import get_db
 from delivery import crud
-from forecast.winters.RunWinters import RunWinters
+from forecast.winters.RunModels import RunModels
 
 router = APIRouter(
     prefix="/forecasts_and_deliveries",
@@ -16,7 +16,7 @@ router = APIRouter(
 @router.get("/{station_id}")
 def get_forecasts_and_deliveries(station_id: int, db: Session = Depends(get_db)):
     data = crud.read_deliveries_for_winters(station_id=station_id, limit=350, db=db)
-    run_winters = RunWinters()
-    forecasts = run_winters.for_every_fuel(data)
+    run_winters = RunModels()
+    forecasts = run_winters.holt_winters_for_every_fuel(data)
     deliveries = crud.read_all_deliveries(db)
     return deliveries+forecasts
