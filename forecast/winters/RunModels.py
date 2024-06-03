2b86cb0ac78779de2142a80cc6d9ad8e28ccbfa4
Bartosz Bizoń <bartosz.bizon17@gmail.com>
2024-06-03 12:54:32 +0200

    Changes due to using HoltWinters instead of Winters

---
+++ forecast/winters/RunModels.py
@@ -0,0 +1,29 @@
+import logging
+
+from delivery.model import Delivery as DeliveryModel
+from forecast.winters.HoltWinters import HoltWinters
+from forecast.winters.WintersDataConverter import WintersDataConverter
+from helpers.ConsoleLogger import ConsoleLogger
+
+
+class RunModels:
+    def __init__(self):
+        self.logger = ConsoleLogger()
+
+    def holt_winters_for_every_fuel(self, delivery_models: list[DeliveryModel], forecast_len: int):
+        data_converter = WintersDataConverter(delivery_models)
+        data_converter.convert_zeros_to_small_floats()
+
+        forecasts = {}
+
+        for fuel in data_converter.series_dict.keys():
+            self.logger.log(level=logging.INFO, msg=f"Starting HoltWinters for series \"{fuel}\".")
+
+            holt_winters = HoltWinters(data_converter.series_dict[fuel])
+            holt_winters.fit()
+
+            forecasts[fuel] = holt_winters.get_forecast(forecast_len)
+
+            # self.logger.log(level=logging.INFO, msg="HoltWinters run successfully!")
+
+        return data_converter.cast_forecasts_to_forcast_models(forecasts)
--- forecast/winters/WintersDataConverter.py
+++ forecast/winters/WintersDataConverter.py
@@ -1,4 +1,5 @@
 import datetime
+import pandas as pd
 
 from delivery.dto import Delivery as DeliveryDto
 from delivery.dto import Forecast as ForecastDto
@@ -8,6 +9,7 @@ from delivery.model import Delivery as DeliveryModel
 class WintersDataConverter:
     def __init__(self, delivery_models: list[DeliveryModel]):
         self.delivery_models: list[DeliveryModel] = delivery_models
+
         self.deliveries: list[DeliveryDto] = []
         self.cast_delivery_models_to_deliveries()
         self.deliveries.sort(key=lambda d: d.date)
@@ -16,7 +18,7 @@ class WintersDataConverter:
         self.set_series_count()
 
         self.series_dict = {}
-        self.set_series_dict_from_deliveries()
+        self.set_series_dict()
 
     def cast_delivery_models_to_deliveries(self):
         if len(self.deliveries) > 0:
@@ -37,6 +39,27 @@ class WintersDataConverter:
             )
             self.deliveries.append(delivery)
 
+    def set_series_count(self):
+        self.series_count = len(self.deliveries[0].get_delivery_for_every_fuel())
+
+    def set_series_dict(self):
+        fuel_keys = self.deliveries[0].get_delivery_for_every_fuel().keys()
+        self.series_dict = {key: pd.Series() for key in fuel_keys}
+
+        for i in range(len(self.deliveries)):
+            single_delivery = self.deliveries[i]
+            delivery_for_every_fuel = single_delivery.get_delivery_for_every_fuel()
+
+            for fuel in fuel_keys:
+                self.series_dict[fuel] = (
+                    pd.concat([self.series_dict[fuel], pd.Series(delivery_for_every_fuel[fuel])],
+                          axis=0, ignore_index=True)
+                )
+
+    def convert_zeros_to_small_floats(self):
+        for fuel in self.series_dict.keys():
+            self.series_dict[fuel] = self.series_dict[fuel].map(lambda x: float(1e-4) if x == 0 else x)
+
     def cast_forecasts_to_forcast_models(self, forecasts: dict):
         forecast_len = len(forecasts["ulg95"])
         forecast_models = []
@@ -46,29 +69,24 @@ class WintersDataConverter:
 
         date_newest_delivery = min([delivery.date for delivery in self.deliveries])
 
-        for i in forecasts["ulg95"]:
-            date = date_newest_delivery + datetime.timedelta(days=i-12)
+        forecasts = self.fillna_in_forecasts(forecasts)
+
+        for i in range(forecast_len):
+            date = date_newest_delivery + datetime.timedelta(days=i)
             forecast = ForecastDto(
                 station_id=self.delivery_models[0].station_id,
                 date=date.strftime("%Y-%m-%d"),
-                ulg95_forecast=forecasts["ulg95"][i],
-                dk_forecast=forecasts["dk"][i],
-                ultsu_forecast=forecasts["ultsu"][i],
-                ultdk_forecast=forecasts["ultdk"][i],
+                ulg95_forecast=forecasts["ulg95"].iloc[i],
+                dk_forecast=forecasts["dk"].iloc[i],
+                ultsu_forecast=forecasts["ultsu"].iloc[i],
+                ultdk_forecast=forecasts["ultdk"].iloc[i],
                 id=i)
             forecast_models.append(forecast)
         return forecast_models
 
-    def set_series_count(self):
-        self.series_count = len(self.deliveries[0].get_delivery_for_every_fuel())
+    @staticmethod
+    def fillna_in_forecasts(forecasts: dict):
+        for fuel in forecasts.keys():
+            forecasts[fuel] = forecasts[fuel].fillna(0)
 
-    def set_series_dict_from_deliveries(self):
-        fuel_keys = self.deliveries[0].get_delivery_for_every_fuel().keys()
-        self.series_dict = {key: {} for key in fuel_keys}
-
-        for i in range(len(self.deliveries)):
-            single_delivery = self.deliveries[i]
-            delivery_for_every_fuel = single_delivery.get_delivery_for_every_fuel()
-
-            for key in fuel_keys:
-                self.series_dict[key][i + 1] = delivery_for_every_fuel[key]
+        return forecasts
