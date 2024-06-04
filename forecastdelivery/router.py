a218a529eb3ac5f3d1a069ac98e370d7c338082f
Bartosz Bizoń <bartosz.bizon17@gmail.com>
2024-06-04 20:15:09 +0200

    Get delivery_dtos and forecasts in get_forecasts_and_deliveries

--- forecastdelivery/router.py
+++ forecastdelivery/router.py
@@ -5,7 +5,7 @@ from sqlalchemy.orm import Session
 from database.database import get_db
 from delivery import crud
 from forecast.router import get_forecasts
-from forecast.winters.DataConverter import DataConverter
+from helpers.ModelDtoCasting import ModelDtoCasting
 
 router = APIRouter(
     prefix="/forecasts_and_deliveries",
@@ -17,9 +17,8 @@ router = APIRouter(
 @router.get("/")
 def get_forecasts_and_deliveries(station_id: int, forecast_len: int, db: Session = Depends(get_db)):
     delivery_models = crud.read_latest_deliveries_for_station(db, station_id, forecast_len)
-    deliveries_converted = DataConverter(delivery_models)
+    delivery_dtos = ModelDtoCasting.delivery_model_to_delivery_dto(delivery_models)
 
     forecasts = get_forecasts(station_id, forecast_len, db)
 
-
-    return deliveries_converted.deliveries
+    return forecasts + delivery_dtos
