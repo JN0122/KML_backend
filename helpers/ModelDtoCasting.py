49dff87c0ae1fcf3e03f5adfba448f260b6f04ed
Bartosz Bizoń <bartosz.bizon17@gmail.com>
2024-06-04 20:11:55 +0200

    Create class for model-dto casting

---
+++ helpers/ModelDtoCasting.py
@@ -0,0 +1,18 @@
+from delivery.model import Delivery as DeliveryModel
+from delivery.dto import Delivery as DeliveryDto
+
+
+class ModelDtoCasting:
+    @staticmethod
+    def delivery_model_to_delivery_dto(delivery_model: DeliveryModel):
+        return DeliveryDto(
+            station_id=delivery_model.station_id,
+            date=delivery_model.date,
+            time=delivery_model.time,
+            ulg95=delivery_model.ulg95,
+            dk=delivery_model.dk,
+            ultsu=delivery_model.ultsu,
+            ultdk=delivery_model.ultdk,
+            id=delivery_model.id,
+            total=delivery_model.total
+        )
