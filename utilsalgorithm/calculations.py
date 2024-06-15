2e82976d7f32dfcfd5f7caf262b6be15c8492393
alien2fg <filipgnojek@gmail.com>
2024-06-16 00:51:42 +0200

    Adding brake and oil algorithm

---
+++ utilsalgorithm/calculations.py
@@ -0,0 +1,11 @@
+def calculate_trips_left(distance: float, brake_pads_km_left: int, oil_change_km_left: int) -> int:
+    if distance <= 0:
+        raise ValueError("Distance must be greater than zero.")
+    
+    one_trip_distance = 2 * distance
+    
+    brake_pads_trips_left = brake_pads_km_left // one_trip_distance
+    
+    oil_change_trips_left = oil_change_km_left // one_trip_distance
+    
+    return min(brake_pads_trips_left, oil_change_trips_left)
