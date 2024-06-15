7df2151c1c98876fe6334021d7fd7829fb97d897
alien2fg <filipgnojek@gmail.com>
2024-06-16 00:32:45 +0200

    Adding truck and station router integration

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ from database.database import engine, Base, get_session
 from delivery import router as delivery_router
 from forecast import router as forecast_router
 from forecastdelivery import router as forecastdelivery_router
+from truck.router import router as truck_router
+from station.router import router as station_router
 
 Base.metadata.create_all(bind=engine)
 
@@ -14,6 +16,8 @@ app.include_router(delivery_router.router)
 app.include_router(forecast_router.router)
 app.include_router(forecastdelivery_router.router)
 app.include_router(db_router.router)
+app.include_router(truck_router)
+app.include_router(station_router)
 
 # run on start
 db_router.seed_database(get_session())
